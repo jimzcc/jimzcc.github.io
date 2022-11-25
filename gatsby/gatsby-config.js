a13c801a938e22831af0f435f60e6e0e4319eac4
JIMz@dev3-yuco <jimz@yucolab.com>
2022-11-25 16:03:50 +0800

    gatsby starter with minor changes

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: 'pages.jimz.cc',
+    description: 'pages.jimz.cc on github pages, with gatsby starter',
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://pages.jimz.cc/`,
   },
   plugins: [
     `gatsby-plugin-image`,
